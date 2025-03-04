41af6f88bdf7ffebc281294c316d29ee8f01f73b
ahmad-v0 <ahmad.prov0@gmail.com>
2025-03-04 23:40:59 +0600

    Code updated for click functionality, properly commented a function is coded to calculate the total number of pending tasks, show the results on top, update the number whenever new task is added, checkbox of the list is checked or unchecked and when the page is loaded for the first time or again; coded for item removal whenever the trash icon is clicked, and update the total number of pending task.

--- script-revised.js
+++ script-revised.js
@@ -1,32 +1,41 @@
-
 var inputForm = document.querySelector('form');
 var inputBox = document.querySelector('.todo-input');
 var taskList = document.querySelector('.todo-list');
 var inputTxt;
 
-var today = new Date();
-document.getElementById('date').innerText = today.toDateString();
+var today = new Date();                                                 // a function in variable to catch the current date of user browser
+document.getElementById('date').innerText = today.toDateString();       // to show the date as a string of day month day year
 
-
-inputForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    inputTxt = inputBox.value;
-    inputBox.value = '';
-    if (inputTxt !== "") {
-        addNewTask();
+inputForm.addEventListener('submit', function(event) {                  // add a evenlistener to the from, for whenever enter key is pressed of plus icon is clicked, will invoke a function
+    event.preventDefault();                                             // prevent the default behavior of event, as once form is submitted, the page will reload and value on inputBox will be cleared
+    inputTxt = inputBox.value;                                          // store the input into a variable
+    inputBox.value = '';                                                // manual removal of data from input text
+    if (inputTxt !== "") {                                              // input validation, to prevent blank items to be stored as input
+        addNewTask();                                                   // add new item to the list
     }
 });
 
-document.addEventListener('change', function(event) {
-    if (event.target.classList.contains('itemCheck')) {
+document.addEventListener('DOMContentLoaded', function() {              // eventlistener to invoke an annonymous function once the content is loaded
+    pendingTaskCal();                                                   // calculate the number of pending task
+});
+
+document.addEventListener('change', function(event) {                    // an eventlistener to catch the changes on the entire document
+    if (event.target.classList.contains('itemCheck')) {                  // to check if the changes happened on checkboxes on tasks
         pendingTaskCal();
     };
 });
 
-function addNewTask() {
-    var newTask = document.createElement('div');
-    newTask.classList.add('todo');
-    newTask.innerHTML = `
+document.querySelector('.todo-list').addEventListener('click', function(event) {        // select the entire to do list, add an eventlistener for click event
+    if (event.target.classList.contains('trash')) {                                     // checks if the trash icon is clicked
+        event.target.closest('.todo').remove();                                         // removes the closest todo class of the trash icon, which is the parent element
+        pendingTaskCal();
+    }
+})
+
+function addNewTask() {                                                    // a function that when invoked, add new task to the task list
+    var newTask = document.createElement('div');                           // create a new div element
+    newTask.classList.add('todo');                                         // add the created div in to todo class
+    newTask.innerHTML = `                                                   
                 <p class="todo-item">
                     <span><i class="fas fa-bars"></i></span> 
                     ${inputTxt}
@@ -40,22 +49,22 @@ function addNewTask() {
                         <i class="fas fa-trash"></i>
                     </button>
                 </div>
-    `;
-    taskList.appendChild(newTask);
+    `;                                                                       // html elements added to newly created div
+    taskList.appendChild(newTask);                                           // entire div is added to existing task list
     // var taskOnTop = document.querySelector('todo');
     // taskList.insertBefore(taskOnTop);
     pendingTaskCal();
 }
 
-function pendingTaskCal() {
-    var taskOnList = document.querySelectorAll('.itemCheck');
-    var pendingTask = 0;
-    for (index = 0; index < taskOnList.length; index++) {
+function pendingTaskCal() {                                                 // a function that when invoked, calculates the total number of pending task on task list
+    var taskOnList = document.querySelectorAll('.itemCheck');               // select all the tasks on task list
+    var pendingTask = 0;                                                    // set the number of pending task as zero
+    for (index = 0; index < taskOnList.length; index++) {                   // loops through the tasks on task list
         var taskToCheck = taskOnList[index];
-        if (!taskToCheck.checked) {
-            pendingTask++;
+        if (!taskToCheck.checked) {                                         // checks if the checkbox of the task is unchecked
+            pendingTask++;                                                  // increment pendingtask
         }
     }
     // console.log(pendingTask);
-    document.getElementById('pending-task').innerText = pendingTask;
+    document.getElementById('pending-task').innerText = pendingTask;        
 }
